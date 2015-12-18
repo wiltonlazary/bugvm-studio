55dce9ec9c88e2f39f0087337485617934f7f479
ibinti <github@ibinti.com>
2015-12-18 07:18:47 -0800

    idea/143.1511

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-ANDROID_PLUGIN_COMMIT=46f7904a9d8ce097136a942151724ffd51bfba12
-ANDROID_TOOLS_BASE_COMMIT=4a46ed4b0524fc6389962de7fa3d79f69c6193a6
 rm -rf android
 git clone git://git.jetbrains.org/idea/android.git android
 cd android
 git pull
-git checkout $ANDROID_PLUGIN_COMMIT
+git checkout idea/143.1511
 cd ..
 git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
 cd android/tools-base
 git pull
-git checkout $ANDROID_TOOLS_BASE_COMMIT
+git checkout idea/143.1511
 cd ../..
 
 version=$BUGVM_IDEA_PLUGIN_VERSION
